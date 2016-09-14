40ae317d10a4bc034a24344f52817eb9bc8bd463
João Pinho <joao.pinho@tlantic.com>
2016-09-14 11:06:04 +0100

    refactoring(couchbaselib) 2.0 MRS-202 #resolved #time 3d #comment Reformulação da lib.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib');
+/**
+ * @module couchbaselib
+ */
+'use strict';
+
+const
+	CouchBaseEnvironment  = require('./lib/environment');
+
+
+module.exports = new CouchBaseEnvironment();
